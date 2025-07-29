bc6c9376fef92a4e7abb68fd50ddaee68112eb09
Noro-dev-go <tolstoyandrey9@gmail.com>
2025-07-29 14:14:30 +0300

    new contacts command has been added

--- utils/bot_commands.py
+++ utils/bot_commands.py
@@ -4,5 +4,6 @@ async def setup_bot_commands(bot):
     await bot.set_my_commands([
         BotCommand("start", "Перезапустить бота"),
         BotCommand("menu", "Вернуться в меню"),
-        BotCommand("help", "Помощь при зависании")
+        BotCommand("help", "Помощь при зависании"),
+        BotCommand("contacts", "Контакты")
     ])
